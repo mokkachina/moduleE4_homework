5962490996b7bec818df2f65681bb4e9dd57b906
root <maxiklim80@gmail.com>
2023-04-19 15:20:16 +0600

    add_task №5

--- Задание5.js
+++ Задание5.js
@@ -1,7 +1,62 @@
-const proba = {a:1, b:2} 
-function value (obj){ 
-for (let key in obj) { 
-if (obj.hasOwnProperty(key)) { 
-console.log(key, obj[key]); } 
-} 
-} value(proba);
+class Device {
+    constructor(name) {
+    this.name = name;
+    }
+  
+  
+  
+    electricDeviceWork () {
+      let work = false
+      if (this.electric && this.switchon) {
+        work = true;
+      } else {
+           work = false;
+               }
+     return (work);
+     }
+  }
+  class EleDevice extends Device{
+    
+  constructor (name, power, electric, switchon) {
+    super(name);
+    this.power = power;
+    this.electric = electric;
+    this.switchon = switchon;
+    this. showPower = function(){
+      console.log(name)
+    }
+  }
+  
+  }
+  
+  
+  
+  let sumPower = function(){
+      let sum = 0;
+     for(let value of ElecDiv){
+      if (value.electricDeviceWork()){
+          sum += value.power;
+      }else{
+          sum;
+      }
+      
+      
+  
+     }
+    return sum;
+   
+  
+  }
+  
+  
+  const lamp = new EleDevice(name = "lampa", power = 30, electric = true, switchon = true);
+  const comp = new EleDevice(name = "compyter", power = 20, electric = true, switchon = true);
+  const flight = new EleDevice(name = "flashlight", power = 10, electric = true, switchon = false);
+  let ElecDiv = [lamp, comp, flight];
+  
+  console.log(lamp.electricDeviceWork());
+  
+  console.log(sumPower(ElecDiv));
+  lamp.showPower();
+  comp.showPower();
+  
